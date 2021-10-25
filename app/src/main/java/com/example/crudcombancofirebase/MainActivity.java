092179f5e9a1bbe9ac2e29184020298e90e0dd18
kleberfort <kleberfort@gmail.com>
2021-10-25 16:19:50 -0300

    adicionado o código de cadastro de usuário no firebase

--- app/src/main/java/com/example/crudcombancofirebase/MainActivity.java
+++ app/src/main/java/com/example/crudcombancofirebase/MainActivity.java
@@ -3,14 +3,71 @@ package com.example.crudcombancofirebase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.crudcombancofirebase.model.Usuario;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private String nome, sobrenome, email;
+    private int contato;
+    private EditText editNome, editSobrenome, editEmail, editContato;
+    private Button btnCriar;
+
+    private DatabaseReference banco  = FirebaseDatabase.getInstance().getReference();
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        editNome = findViewById(R.id.editName);
+        editSobrenome = findViewById(R.id.editSobrenome);
+        editEmail = findViewById(R.id.editEmail);
+        editContato = findViewById(R.id.editContato);
+        btnCriar = findViewById(R.id.btnCriar);
+
+
+
+
+
+
+
+        btnCriar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                DatabaseReference usuarios = banco.child("usuarios");
+                Usuario usuario = new Usuario();
+
+
+                nome = editNome.getText().toString();
+                sobrenome = editSobrenome.getText().toString();
+                email = editEmail.getText().toString();
+                contato = Integer.parseInt(editContato.getText().toString());
+
+                usuario.setNome(nome);
+                usuario.setSobrenome(sobrenome);
+                usuario.setEmail(email);
+                usuario.setContato(contato);
+
+                usuarios.push().setValue(usuario);
+
+                Toast.makeText(getApplicationContext(), "Usuário cadastrado", Toast.LENGTH_LONG).show();
+
+            }
+        });
+
     }
+
+
+
 }
